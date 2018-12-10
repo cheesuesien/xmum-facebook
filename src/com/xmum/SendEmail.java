959f6d7a920a05ebb38878afa18e3c9b22aeffd4
Chee Sue Sien <cheesuesien@gmail.com>
2018-12-10 15:21:03 +0800

    tidying up and deleting unnecessary files

---
+++ src/com/xmum/SendEmail.java
@@ -0,0 +1,65 @@
+package com.xmum;
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+    public class SendEmail {
+
+        public static void main(String [] args) {
+            // Recipient's email ID needs to be mentioned.
+            String to = "cdtomgreen@gmail.com";
+
+            // Sender's email ID needs to be mentioned
+            String from = "cdtomgreen@gmail.com";
+
+            // Assuming you are sending email from localhost
+            String host = "localhost";
+
+            // Get system properties
+            Properties properties = System.getProperties();
+
+            // Setup mail server
+            properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+            properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            properties.setProperty("mail.smtp.socketFactory.fallback", "false");
+            properties.setProperty("mail.smtp.port", "465");
+            properties.setProperty("mail.smtp.socketFactory.port", "465");
+            properties.put("mail.smtp.auth", "true");
+            properties.put("mail.debug", "true");
+            properties.put("mail.store.protocol", "pop3");
+            properties.put("mail.transport.protocol", "smtp");
+
+            // Get the default Session object.
+            Session session = Session.getDefaultInstance(properties,
+                    new Authenticator(){
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication("", "");
+                        }});
+
+            try {
+                // Create a default MimeMessage object.
+                MimeMessage message = new MimeMessage(session);
+
+                // Set From: header field of the header.
+                message.setFrom(new InternetAddress(from));
+
+                // Set To: header field of the header.
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+                // Set Subject: header field
+                message.setSubject("This is the Subject Line!");
+
+                // Now set the actual message
+                message.setText("This is actual message");
+
+                // Send message
+                Transport.send(message);
+                System.out.println("Sent message successfully....");
+            } catch (MessagingException mex) {
+                mex.printStackTrace();
+            }
+        }
+    }
+
